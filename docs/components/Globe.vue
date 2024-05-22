6f365ab86166dfcc93644fdf88572576197dcc1e
Arash <arashi.sheyda@gmail.com>
2024-05-22 15:54:05 -0600

    use points instead of markers

--- docs/components/Globe.vue
+++ docs/components/Globe.vue
@@ -12,9 +12,10 @@ import countries from './files/globe-data.json'
 const arcs = 13
 const element = ref()
 const colorMode = useColorMode()
-let renderers, Globe, scene, camera, controls
+let renderers, scene, camera, controls
 
-const marker = '<svg viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M281.44 397.667H438.32C443.326 397.667 448.118 395.908 452.453 393.427C456.789 390.946 461.258 387.831 463.76 383.533C466.262 379.236 468.002 374.36 468 369.399C467.998 364.437 466.266 359.563 463.76 355.268L357.76 172.947C355.258 168.65 352.201 165.534 347.867 163.053C343.532 160.573 337.325 158.813 332.32 158.813C327.315 158.813 322.521 160.573 318.187 163.053C313.852 165.534 310.795 168.65 308.293 172.947L281.44 219.587L227.733 129.13C225.229 124.834 222.176 120.307 217.84 117.827C213.504 115.346 208.713 115 203.707 115C198.701 115 193.909 115.346 189.573 117.827C185.238 120.307 180.771 124.834 178.267 129.13L46.8267 355.268C44.3208 359.563 44.0022 364.437 44 369.399C43.9978 374.36 44.3246 379.235 46.8267 383.533C49.3288 387.83 53.7979 390.946 58.1333 393.427C62.4688 395.908 67.2603 397.667 72.2667 397.667H171.2C210.401 397.667 238.934 380.082 258.827 346.787L306.88 263.4L332.32 219.587L410.053 352.44H306.88L281.44 397.667ZM169.787 352.44H100.533L203.707 174.36L256 263.4L221.361 323.784C208.151 345.387 193.089 352.44 169.787 352.44Z" fill="#00DC82"/></svg>'
+let Globe: ThreeGlobe
+
 const globeSize = [
   {
     breakpoint: 1024,
@@ -43,7 +44,7 @@ function init() {
   renderers.forEach((r, idx) => {
     r.setSize(globeSize[0].size, globeSize[0].size)
     if (idx === 0) {
-      r.setPixelRatio(window.devicePixelRatio / 2)
+      r.setPixelRatio(window.devicePixelRatio)
       // set BG transparent
       r.setClearColor(0x000000, 0)
     } else {
@@ -124,17 +125,26 @@ function initGlobe() {
       .arcDashInitialGap(() => Math.random() * 5)
       .arcDashAnimateTime(1000)
       .arcsTransitionDuration(1000)
-      // markers
-      .htmlElementsData(arcsData.flatMap(arc => [
-        { lat: arc.startLat, lng: arc.startLng },
-        { lat: arc.endLat, lng: arc.endLng },
+      // points
+      .pointsData(arcsData.flatMap(arc => [
+        { lat: arc.startLat, lng: arc.startLng, },
+        { lat: arc.endLat, lng: arc.endLng, },
       ]))
-      .htmlElement(() => {
-        const el = document.createElement('div')
-        el.innerHTML = marker
-        el.style.width = '20px'
-        return el
-      })
+      .pointColor(() => '#F89D35')
+      .pointAltitude(0.001)
+      .pointRadius(1)
+      // markers for parsing html elements
+      // .htmlElementsData(arcsData.flatMap(arc => [
+      //   { lat: arc.startLat, lng: arc.startLng, isOdd: 0 },
+      //   { lat: arc.endLat, lng: arc.endLng, isOdd: 1 },
+      // ]))
+      // .htmlElement((item) => {
+      //   const el = document.createElement('div')
+      //   el.innerHTML = item.isOdd ? marker : cloudflare
+      //   el.style.color = item.isOdd ? '#00DB86' : '#f38020'
+      //   el.style.width = '20px'
+      //   return el
+      // })
   }, 100)
 
   const globeMaterial = Globe.globeMaterial()
@@ -156,22 +166,10 @@ function initGlobe() {
 
   scene.add(Globe)
 
-  // const WireFrameGlobe = new ThreeGlobe({
-  //   waitForGlobeReady: true,
-  //   animateIn: true,
-  // })
-
-  // const wireFrameMaterial = WireFrameGlobe.globeMaterial()
-  // wireFrameMaterial.wireframe = true
-  // wireFrameMaterial.color = new Color(0x00DC82)
-
-  // wireFrameMaterial.transparent = true
-  // wireFrameMaterial.opacity = 0.3
-
-  // scene.add(WireFrameGlobe)
-
   // Update pov when camera moves
+  // @ts-ignore
   Globe.setPointOfView(camera.position, Globe.position)
+  // @ts-ignore
   controls.addEventListener('change', () => Globe.setPointOfView(camera.position, Globe.position))
 }
 
